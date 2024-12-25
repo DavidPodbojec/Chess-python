0831ffbe3ceb9c370545726f258922adca7c58de
DavidPodbojec <podbojec.david@gmail.com>
2024-12-25 13:37:37 +0100

    black horse move complete, black rook move start

--- black.py
+++ black.py
@@ -36,10 +36,39 @@ def black_possible_moves(board, selected_piece):
             possible_moves.append([selected_piece[0]+1, selected_piece[1]-2])
             
         #right up
+        if selected_piece[0] >= 1 and selected_piece[1] <= 5 and board[selected_piece[0]-1][selected_piece[1]+2] % 10 == 0:
+            possible_moves.append([selected_piece[0]-1, selected_piece[1]+2])
+            
+        #right down
+        if selected_piece[0] <= 6 and selected_piece[1] <= 5 and board[selected_piece[0]+1][selected_piece[1]+2] % 10 == 0:
+            possible_moves.append([selected_piece[0]+1, selected_piece[1]+2])
+            
+        #up left
+        if selected_piece[0] >= 2 and selected_piece[1] >= 1 and board[selected_piece[0]-2][selected_piece[1]-1] % 10 == 0:
+            possible_moves.append([selected_piece[0]-2, selected_piece[1]-1])
+            
+        #up right 
+        if selected_piece[0] >= 2 and selected_piece[1] <= 6 and board[selected_piece[0]-2][selected_piece[1]+1] % 10 == 0:
+            possible_moves.append([selected_piece[0]-2, selected_piece[1]+1])
         
     
     def rook_move():
-        print("black rook")
+        #down
+        print(selected_piece[0])
+        count = 8 - selected_piece[0]
+        
+        for i in range(count):
+            if board[selected_piece[0]+i][selected_piece[1]] % 10 == 0:
+                break
+            elif board[selected_piece[0]+i][selected_piece[1]] % 10 == 0 and board[selected_piece[0]+i][selected_piece[1]] == 0:
+                possible_moves.append([selected_piece[0]+i, selected_piece[1]])
+            
+            elif board[selected_piece[0]+i][selected_piece[1]] % 10 == 0 and board[selected_piece[0]+i][selected_piece[1]] != 0:
+                possible_moves.append([selected_piece[0]+i, selected_piece[1]])
+                break
+            
+            
+            
     
     def bishop_move():
         print("black bishop")
