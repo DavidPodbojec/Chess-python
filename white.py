e798810f5ebdec7f6289933b81708363a690a280
DavidPodbojec <podbojec.david@gmail.com>
2024-12-27 12:52:41 +0100

    white bishop down left move done

--- white.py
+++ white.py
@@ -104,7 +104,59 @@ def white_possible_moves(board, selected_piece):
             possible_moves.append([selected_piece[0]-2, selected_piece[1]+1])
     
     def bishop_move():
-        print("white bishop")
+        #down left
+        if selected_piece[0] != 7 and selected_piece[1] != 0:
+            count = 2
+            if min(8-selected_piece[0], selected_piece[1]+1) > 1:
+                count = min(8-selected_piece[0], selected_piece[1]+1)
+                
+            
+            for i in range(1, count):
+                if board[selected_piece[0]+i][selected_piece[1]-i] == 0: 
+                    possible_moves.append([selected_piece[0]+i, selected_piece[1]-i])
+                elif board[selected_piece[0]+i][selected_piece[1]-i] % 10 != 0:
+                    possible_moves.append([selected_piece[0]+i, selected_piece[1]-i])
+                    break                   
+                
+                else:
+                    break
+            
+        #down right  
+        if selected_piece[0] != 7 and selected_piece[1] != 7:
+            for i in range(1, min(8-selected_piece[0], 8-selected_piece[1])):
+                if board[selected_piece[0]+i][selected_piece[1]+i] == 0: 
+                    possible_moves.append([selected_piece[0]+i, selected_piece[1]+i])
+                elif board[selected_piece[0]+i][selected_piece[1]+i] % 10 != 0:
+                    possible_moves.append([selected_piece[0]+i, selected_piece[1]+i])
+                    break
+                    
+                else:
+                    break
+                    
+        #up left
+        if selected_piece[0] != 0 and selected_piece[1] != 0:
+            for i in range(1, min(selected_piece[0], selected_piece[1])):
+                if board[selected_piece[0]-i][selected_piece[1]-i] == 0:
+                    possible_moves.append([selected_piece[0]-i, selected_piece[1]-i])
+                elif board[selected_piece[0]-i][selected_piece[1]-i] % 10 != 0:
+                    possible_moves.append([selected_piece[0]-i, selected_piece[1]-i])
+                    break
+                    
+                else:
+                    break
+                    
+        #up right 
+        if selected_piece[0] != 0 and selected_piece[1] != 7:
+            for i in range(1, min(selected_piece[0], 8-selected_piece[1])):
+                if board[selected_piece[0]-i][selected_piece[1]+i] == 0:
+                    possible_moves.append([selected_piece[0]-i, selected_piece[1]+i])
+                elif board[selected_piece[0]-i][selected_piece[1]+i] % 10 != 0:
+                    possible_moves.append([selected_piece[0]-i, selected_piece[1]+i])
+                    break
+
+                else:
+                    break
+            
     
     def queen_move():
         print("white queen")
